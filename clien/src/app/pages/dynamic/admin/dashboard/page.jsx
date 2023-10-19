b8d1f79eba94820fe652963847abdb1fe9f986bb
harsha <harshasathsarani@gmail.com>
2023-10-20 00:12:11 +0530

    final chngs

---
+++ clien/src/app/pages/dynamic/admin/dashboard/page.jsx
@@ -0,0 +1,10 @@
+
+import "./page.css"
+
+export default function AdminDashboard(){
+    return(
+        <div className="container-fluid vh-100">
+            <h3 className="pt-3 text-center">Dashboard</h3>
+        </div>
+    )
+}
